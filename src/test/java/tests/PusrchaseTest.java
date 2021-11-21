5de11075975838dde4ded37d8b7dad28607a86e2
Kateryna Roi <kateryna782@gmail.com>
2021-11-21 18:28:21 -0500

    Zilola- update 3

--- src/test/java/tests/PusrchaseTest.java
+++ src/test/java/tests/PusrchaseTest.java
@@ -8,13 +8,14 @@ import pages.PurchasePage;
 public class PusrchaseTest extends TestBase {
 
     @Test(groups = {"regressionTest"})
-    public void checkPurchaseRates() {
+    public void checkPurchaseRatesforNewCarBuyingServise() {
 
         AutoLoanApplicationPage autoLoanApplicationPage = new AutoLoanApplicationPage();
         autoLoanApplicationPage.NavigateToAutoRatesPage();
         AutoRefinancePage autoRefinancePage = new AutoRefinancePage();
         autoRefinancePage.purchaseButton.click();
         PurchasePage purchasePage = new PurchasePage();
+
 
 
     }
