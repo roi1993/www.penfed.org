3a3ec9a161dafea668bd578dd50fd0e8be8b77c7
Konul Salimova <salimovakonul@gmail.com>
2021-11-22 14:59:35 -0500

    NavigateCreditCardPage passed

--- src/test/java/tests/CreditCardTest.java
+++ src/test/java/tests/CreditCardTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.CookiesHandlingPage;
 import pages.CreditCardPage;
 import utilities.ConfigReader;
 
@@ -10,23 +12,39 @@ import static org.openqa.selenium.Keys.TAB;
 
 
 public class CreditCardTest extends TestBase {
-        WebDriver driver;
+    WebDriver driver;
 
-        public void navigateCreditCardOverviewPage() {
-            CreditCardPage cd = new CreditCardPage();
+    public void navigateCreditCardOverviewPage() {
+        CreditCardPage cd = new CreditCardPage();
 
-            cd.creditCards.sendKeys(TAB);
-            cd.creditCardOverview.click();
-            cd.calculateBalanceTransfer.click();
-            cd.amount.sendKeys(ConfigReader.getProperty("7500"+ Keys.TAB));
-            cd.currentInterestRate.sendKeys(ConfigReader.getProperty("18"));
-            cd.calculateButton.click();
+        cd.creditCards.sendKeys(TAB);
+        cd.creditCardOverview.click();
+        cd.calculateBalanceTransfer.click();
+        cd.amount.sendKeys(ConfigReader.getProperty("7500" + Keys.TAB));
+        cd.currentInterestRate.sendKeys(ConfigReader.getProperty("18"));
+        cd.calculateButton.click();
 
-
-        }
-//
-//        @Test
-//    public void
 
     }
+
+    @Test
+    public void navigateCreditCardPage() {
+        CookiesHandlingPage cookiesHandlingPage = new CookiesHandlingPage();
+        cookiesHandlingPage.click();
+        CreditCardPage cd = new CreditCardPage();
+        cd.creditCards.sendKeys(TAB);
+//        cd.creditCardOverview.click();
+
+//        String expectedPage =
+//        Assert.assertEquals();
+//        String amount = "7500";
+//        String interestRate = "18%";
+
+
+
+
+
+    }
+
+}
 
